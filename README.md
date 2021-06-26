6ebb5125d2837142ec477e38ce3a7a442c8ed06d
Jiayu Liu <jiayu.liu@airbnb.com>
2021-06-26 14:12:50 +0800

    update project with option to send to stdout

--- README.md
+++ README.md
@@ -14,13 +14,25 @@ Use `pip3` if both Python2 and Python3 are installed. This application only work
 
 ## How to use
 
-`parquet_to_csv` converts `parquet` files to `csv` files.
+### Converting Parquet
+
+`parquet_to_csv` converts `parquet` files to `csv` files. By default it prints to the standard
+output, but can be directed via pipe or `-o` flag to write to a file.
 
 ```text
-Usage: parquet_to_csv.py [OPTIONS] INPUT_FILE OUTPUT_PATH
+Usage: parquet_to_csv.py [OPTIONS] INPUT_FILE
 
 Options:
+  -o, --output-path FILE  [default: (standard output)]
   --header / --no-header
   --verbose BOOLEAN
   --help                  Show this message and exit.
 ```
+
+### Selecting columns, `gzip`-ing output
+
+Following UNIX principle, you should be using [xsv](https://github.com/BurntSushi/xsv) for selecting
+columns from the csv or do other transformations: just pipe the output to `xsv` and you're all set.
+
+Similarly if you'd want the file to be compressed, pipe the result to `gzip` and direct to a local
+file ending in `.csv.gz`.
--- parquet_csv/parquet_to_csv.py
+++ parquet_csv/parquet_to_csv.py
@@ -1,5 +1,5 @@
 from time import perf_counter
-
+import sys
 import click
 import pyarrow as pa
 from pyarrow import csv
@@ -8,7 +8,13 @@ from pyarrow import parquet as pq
 
 @click.command()
 @click.argument("input_file", type=click.Path(exists=True, dir_okay=False))
-@click.argument("output_path", type=click.Path(exists=False, dir_okay=False))
+@click.option(
+    "-o",
+    "--output-path",
+    default="-",
+    show_default="standard output",
+    type=click.Path(exists=False, dir_okay=False),
+)
 @click.option("--header/--no-header", default=True)
 @click.option("--verbose", default=True)
 def parquet_to_csv(input_file, output_path, header, verbose):
@@ -16,15 +22,19 @@ def parquet_to_csv(input_file, output_path, header, verbose):
     table = pq.read_table(input_file)
     if verbose:
         click.echo(
-            f"reading from {click.format_filename(input_file)} took {perf_counter() - read_start:.3f} seconds"
+            f"reading from {click.format_filename(input_file)} took {perf_counter() - read_start:.3f} seconds",
+            file=sys.stderr,
         )
     write_start = perf_counter()
     csv.write_csv(
-        table, output_path, write_options=csv.WriteOptions(include_header=header)
+        table,
+        sys.stdout.buffer if output_path == "-" else output_path,
+        write_options=csv.WriteOptions(include_header=header),
     )
     if verbose:
         click.echo(
-            f"writing to {click.format_filename(output_path)} took {perf_counter() - write_start:.3f} seconds"
+            f"writing to {click.format_filename(output_path)} took {perf_counter() - write_start:.3f} seconds",
+            file=sys.stderr,
         )
 
 
